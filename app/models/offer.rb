f65d8bb5218dac7705c825f820de4ee354f843ba
FadiBibawy <eng.fadimichel@gmail.com>
2023-02-23 21:44:21 +0100

    offer can be saved now

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,5 @@
 class Offer < ApplicationRecord
   belongs_to :user
   belongs_to :product
-  belongs_to :offered_product
+  belongs_to :offered_product, class_name: "Product"
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :offers
+  has_many :offer_products, class_name: 'Product', foreign_key: "offered_product_id"
 end
