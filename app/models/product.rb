f192d209d0f203a129be4110f40bd476bbfcc93b
FadiBibawy <eng.fadimichel@gmail.com>
2023-02-24 06:24:26 +0100

    edited some associations between offers and products, to include "offered_offers"

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many :offers
-  has_many :offer_products, class_name: 'Product', foreign_key: "offered_product_id"
+  has_many :offers, dependent: :destroy
+  # has_many :offered_products, class_name: 'Product', foreign_key: "offered_product_id"
+  has_many :offered_offers, class_name: 'Offer', foreign_key: 'offered_product_id', dependent: :destroy
 end
