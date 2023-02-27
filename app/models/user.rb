1b9c81458f289bb97f372ce8a8485d5382fd8900
FadiBibawy <eng.fadimichel@gmail.com>
2023-02-27 01:13:54 +0100

    products controller index and show and new and create done & the seed file for products

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_one_attached :photo
+
   has_many :products
   has_many :offers
   # Include default devise modules. Others available are:
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'products/index'
   devise_for :users
   root to: "pages#home"
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
