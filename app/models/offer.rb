2dda2d98353a23f10dfcebbda0f0a0d9bb4ef105
FadiBibawy <eng.fadimichel@gmail.com>
2023-03-03 03:33:38 +0100

    validated the offer already exists

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,4 +2,22 @@ class Offer < ApplicationRecord
   belongs_to :user
   belongs_to :product
   belongs_to :offered_product, class_name: "Product"
+
+  validate :offered_product_cannot_be_same_as_product
+  validate :unique_products_in_offers
+
+  private
+
+  def offered_product_cannot_be_same_as_product
+    if product == offered_product
+      errors.add(:offered_product, "cannot be the same as the product being offered")
+    end
+  end
+
+  def unique_products_in_offers
+    if Offer.exists?(product_id: product_id, offered_product_id: offered_product_id) ||
+       Offer.exists?(product_id: offered_product_id, offered_product_id: product_id)
+      errors.add(:base, "This offer already exists")
+    end
+  end
 end
