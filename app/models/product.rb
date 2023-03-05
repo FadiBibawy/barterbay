92783cb1b4bc5c28b8849a11dd9da12f3d2b8adc
FadiBibawy <eng.fadimichel@gmail.com>
2023-03-05 16:40:12 +0100

    migration created for products: address

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,7 +11,7 @@ class Product < ApplicationRecord
   # has_many :offered_products, class_name: 'Product', foreign_key: "offered_product_id"
   has_many :offered_offers, class_name: 'Offer', foreign_key: 'offered_product_id', dependent: :destroy
 
-  validates :title, :description, :category, :quality, :photos, presence: true
+  validates :title, :description, :category, :quality, presence: true
 
   pg_search_scope :search_by_title,
                   against: [:title],
