36e896b4ea5b7bf7460a8a3473914d8ea8db2681
FadiBibawy <eng.fadimichel@gmail.com>
2023-03-06 00:08:08 +0100

    removed chatroom from messages

---
+++ db/migrate/20230305230708_remove_chatroom_id_from_messages.rb
@@ -0,0 +1,5 @@
+class RemoveChatroomIdFromMessages < ActiveRecord::Migration[7.0]
+  def change
+    remove_reference :messages, :chatroom, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_05_224633) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_05_230708) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,12 +50,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_05_224633) do
 
   create_table "messages", force: :cascade do |t|
     t.string "content"
-    t.bigint "chatroom_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "offer_id", null: false
-    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
     t.index ["offer_id"], name: "index_messages_on_offer_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
@@ -117,7 +115,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_05_224633) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "offers"
   add_foreign_key "messages", "users"
   add_foreign_key "offers", "products"
