787b6e0f28a9d0a266f0b01306e7fe824f809910
Alexander Wenzel <a.wenzel.berlin@gmx.de>
2023-03-09 20:12:01 +0100

    product cards

--- db/schema.rb
+++ db/schema.rb
@@ -75,15 +75,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_171214) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.string "category"
+    t.string "old_category"
     t.string "quality"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "bartered", default: false
+    t.string "category"
     t.string "subcategory"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
