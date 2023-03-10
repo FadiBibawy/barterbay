56aa84db248276efc222119946f12ee0b0dd4255
FadiBibawy <eng.fadimichel@gmail.com>
2023-03-10 20:08:52 +0100

    added stars to new review form input

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,6 @@ class Review < ApplicationRecord
   belongs_to :reviewer, class_name: "User"
   belongs_to :rated_user, class_name: "User"
 
-  validates :content, presence: true
+  validates :content, :rating, presence: true
   validates :rating, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 1 }
 end
