fa3291b58895821dd81de5189830aec45992a793
OsamaShehata <engosama46@yahoo.com>
2023-03-21 18:19:06 +0100

    stimulus controller to manage the search using ajax

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,9 +7,6 @@ import { application } from "./application"
 import ChatroomSubscriptionController from "./chatroom_subscription_controller"
 application.register("chatroom-subscription", ChatroomSubscriptionController)
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
 import MapController from "./map_controller"
 application.register("map", MapController)
 
@@ -18,3 +15,6 @@ application.register("modal", ModalController)
 
 import ProductFormController from "./product_form_controller"
 application.register("product-form", ProductFormController)
+
+import SearchProductsController from "./search_products_controller"
+application.register("search-products", SearchProductsController)
